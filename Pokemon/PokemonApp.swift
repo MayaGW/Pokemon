44344d93f9db32465cc88114e5dfe5a75e7bd3e7
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-01-24 18:54:48 +0100

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Maya Ghamloush on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
