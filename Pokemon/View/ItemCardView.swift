4f5c6016689b5cdff5d5faab7c77e9d5d5ad4235
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-01-25 10:18:54 +0100

    Create the files of the project and import all resources In Addition to implemet Main grid View with a static data

---
+++ Pokemon/View/ItemCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCardView.swift
+//  Pokemon
+//
+//  Created by Maya Ghamloush on 25/01/2024.
+//
+
+import SwiftUI
+
+struct ItemCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemCardView()
+}
